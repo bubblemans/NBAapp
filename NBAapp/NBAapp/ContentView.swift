42f988125bbe2d8998f25ecbea39983afde7fbb2
bubblemans <f89647779@gmail.com>
2019-10-08 22:26:36 -0700

    replace the wrong project

---
+++ NBAapp/NBAapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBAapp
+//
+//  Created by Alvin Lin on 2019/10/8.
+//  Copyright © 2019 Alvin Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
